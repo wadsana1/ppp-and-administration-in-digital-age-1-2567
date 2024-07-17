68afe9b26d9d76141320b204d67dd319ea6b3424
“wadsana1 <wadsana1109@gmail.com>
2024-07-17 15:34:18 +0700

    first commit

---
+++ src/app/aecranking/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import AECRanking from "../components/AECRanking";
+
+export default function page() {
+  return (
+    <div>
+      <AECRanking />
+    </div>
+  );
+}
---
+++ src/app/ai/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Ai from "../components/Ai";
+
+export default function page() {
+  return (
+    <div>
+      <Ai />
+    </div>
+  );
+}
