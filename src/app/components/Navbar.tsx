59d29f577e23b6bb1f6f317fafe1bf9359cb60f5
“wadsana1 <wadsana1109@gmail.com>
2024-07-17 17:32:09 +0700

    first commit

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -29,7 +29,7 @@ export default function Navbar() {
                 <a href="#">การประเมินนโยบายสาธารณะ</a>
               </li>
               <li className="text-stone-600 hover:text-blue-600 hover:font-bold font-medium mx-4 my-1">
-                <a href="#">ChatGPT</a>
+                <a href="/chatgpt">ChatGPT</a>
               </li>
               <li className="text-stone-600 hover:text-blue-600 hover:font-bold font-medium mx-4 my-1">
                 <a href="/aecranking">AEC Ranking</a>
