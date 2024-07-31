b2d80334f9d968bcfb58d0ba72241cc49c184bc2
Keetadear <keetarat21@gmail.com>
2024-07-31 16:15:41 +0700

    first commit

---
+++ src/app/products/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ProductsList from '@app/components/ProductList'
+
+export default function page() {
+  return (
+    <div>
+        <ProductsList/>
+    </div>
+  )
+}
